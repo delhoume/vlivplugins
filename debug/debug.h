706618d4346bd7464bdf5bd2a022a694cc9e1a55
Frédéric Delhoume <delhoume@gmail.com>
2021-12-28 09:46:14 +0100

    removed existing plugins

--- debug/debug.h
+++
@@ -1,8 +0,0 @@
-#ifndef _VLIV_DEBUG_H
-#define _VLIV_DEBUG_H
-
-#include <vliv.h>
-
-void __declspec(dllexport) RegisterVlivPlugin(ImagePtr img);
-
-#endif
--- lyapunov/lyapunov.h
+++
@@ -1,8 +0,0 @@
-#ifndef _VLIV_LYAPUNOV_H
-#define _VLIV_LYAPUNOV_H
-
-#include <vliv.h>
-
-void __declspec(dllexport) RegisterVlivPlugin(ImagePtr img);
-
-#endif
