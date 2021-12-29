6e38f50a074a77f57a0343efd129809b3e1fe739
Frédéric Delhoume <delhoume@gmail.com>
2021-12-29 10:15:45 +0100

    support for V5 Bmps (32 bit)

--- internal/bmphandler.c
+++ internal/bmphandler.c
@@ -6,8 +6,9 @@
 
 static BOOL AcceptBMPImage(const unsigned char* buffer, unsigned int size) {
     if (size >= 2) {
-	if ((buffer[0] == 'B') && (buffer[1] == 'M'))
+	if ((buffer[0] == 'B') && (buffer[1] == 'M')) {
 	    return TRUE;
+	}
     }
     return FALSE;
 }
@@ -20,6 +21,7 @@ struct bmp_internal {
     unsigned short bitcount;
     unsigned int compression;
     unsigned int paloffset;
+	FILE* file;
 };
 
 static BOOL OpenBMPImage(ImagePtr img, const TCHAR* name) {
@@ -28,7 +30,7 @@ static BOOL OpenBMPImage(ImagePtr img, const TCHAR* name) {
     bmp_internal->offset = 0;
     bmp_internal->paloffset = 0;
     img->numdirs = 1;
-    img->supportmt = 1;
+    img->supportmt = 0;
     img->currentdir = 0;
     return TRUE;
 }
@@ -48,6 +50,7 @@ static unsigned short ReadShort(FILE* file) {
 #define WIN_NEW	    40
 #define OS2_NEW	    64
 #define WIN_OS2_OLD 12
+#define WIN_V5      124
 
 static void SetBMPDirectory(ImagePtr img, unsigned int which) {
     struct bmp_internal* bmp_internal = (struct bmp_internal*)img->handler->internal;
@@ -60,7 +63,7 @@ static void SetBMPDirectory(ImagePtr img, unsigned int which) {
 	ReadInt(file); ReadShort(file); ReadShort(file);
 	offset = ReadInt(file);
 	headersize = ReadInt(file);
-	if ((headersize == WIN_NEW || headersize == OS2_NEW)) {
+	if ((headersize == WIN_NEW || headersize == OS2_NEW || headersize == WIN_V5)) {
 	    img->width = ReadInt(file);
 	    img->height = ReadInt(file);
 	    ReadShort(file); // planes
@@ -70,84 +73,77 @@ static void SetBMPDirectory(ImagePtr img, unsigned int which) {
 	    ReadInt(file); // horizontal resolution
 	    ReadInt(file); // vertical resolution
 	    ReadInt(file); // colors used
-	    ReadInt(file); // important colros
+	    ReadInt(file); // important colors
 	}
+	
 	if (headersize != WIN_OS2_OLD) {
 	    unsigned int bytesleft = headersize - 40;
 	    while (bytesleft--) 
-		fgetc(file);
+			fgetc(file);
 	}
 	switch (bmp_internal->bitcount) {
-	case 1: case 4: case 8: {
-	    RGBQUAD rgb;
-	    unsigned int idx;
-	    unsigned int palsize = 1 << bmp_internal->bitcount;
-	    bmp_internal->paloffset = ftell(file);
-	    for (idx = 0; idx < palsize; ++idx)
-		fread((char*)&rgb, 1, sizeof(RGBQUAD), file);
-	}
+		case 1: case 4: case 8: {
+			RGBQUAD rgb;
+			unsigned int idx;
+			unsigned int palsize = 1 << bmp_internal->bitcount;
+			bmp_internal->paloffset = ftell(file);
+			for (idx = 0; idx < palsize; ++idx) {
+				fread((char*)&rgb, 1, sizeof(RGBQUAD), file);
+			}
+		}
 	}
 	bmp_internal->offset = ftell(file);
+	img->istiled = FALSE;
+	img->numtilesx = 1;
+	img->numtilesy = 1;
 	img->twidth = img->width;
-	img->theight = 256;
-	img->istiled = TRUE;
-	img->numtilesx = img->width / img->twidth;
-	if ((img->numtilesx * img->twidth) <img->width) 
-	    ++img->numtilesx;
-	img->numtilesy = img->height / img->theight;
-	if ((img->numtilesy * img->theight) <img->height) 
-	    ++img->numtilesy;
+	img->theight = img->height;
+	img->subfiletype = Normal;
 	fclose(file);
     }
 }
 
 static void FillBMP(BITMAPINFO* bmi, unsigned int idx, void* arg) {
     struct bmp_internal* bmp_internal = (struct bmp_internal*)arg;
-    RGBQUAD bgra;
-    unsigned int i;
-    FILE* file = (FILE*)arg;
+    FILE* file = (FILE*)bmp_internal->file;
     fseek(file, bmp_internal->paloffset, SEEK_SET);
-    for (i = 0; i < idx; ++i)
-	fread((char*)&bgra, 1, sizeof(RGBQUAD), file);
-    fread((char*)&(bmi->bmiColors[idx]), 1, sizeof(RGBQUAD), file);
+    for (unsigned int i = 0; i < idx; ++i) {
+		fread((char*)&(bmi->bmiColors[idx]), 1, sizeof(RGBQUAD), file);
+	}
 }
 
 static HBITMAP 
 LoadBMPTile(ImagePtr img, HDC hdc, unsigned int x, unsigned int y) {
-    FILE* file = fopen(img->name, "rb");
+   FILE* file = fopen(img->name, "rb");
     HBITMAP hbitmap = 0;
     if (file) {
-	unsigned int* bits = 0;
-	unsigned int realwidth;
-	unsigned int realheight;
-	unsigned int starty = y * img->theight;
-	unsigned int realbottom = img->height - starty;
-	int realtop = realbottom - img->theight;
-	struct bmp_internal* bmpinternal = (struct bmp_internal*)img->handler->internal;
-	unsigned short bitcount = bmpinternal->bitcount;
-	if (realtop <= 0) 
-	    realtop = 0;
-	realheight = realbottom - realtop;
-	realwidth = (((img->twidth * bitcount) + 31) & ~31) >> 3;
-	switch (bitcount) {
-	case 1: case 4: case 8:
-	    if (bmpinternal->compression == BI_RGB)
-		hbitmap = img->helper.CreateIndexedDIBSection(hdc, img->twidth, realheight, &bits, FillBMP, (void*)file);
-	    else {
-		hbitmap = img->helper.CreateDefaultDIBSection(hdc, img->twidth, realheight, "Unsupported compression", &bits);
-		return hbitmap;
-	    }
-	    break;
-	case 16: case 24: case 32: 
-	    hbitmap = img->helper.CreateTrueColorDIBSection(hdc, img->twidth, realheight, &bits, bitcount);
-	    break;
-	}
-	if (bits) {
-	    fseek(file, bmpinternal->offset, SEEK_SET); // goto start of data
-	    fseek(file, realtop * realwidth, SEEK_CUR);  // goto tile start
-	    fread((char*)bits, 1, realheight * realwidth, file); 
-	}
-	fclose(file);
+		unsigned int* bits = 0;
+		struct bmp_internal* bmpinternal = (struct bmp_internal*)img->handler->internal;
+		bmpinternal->file = file;
+		unsigned short bitcount = bmpinternal->bitcount;
+		unsigned int realwidth = (((img->width * bitcount) + 31) & ~31) >> 3;
+		switch (bitcount) {
+			case 1: case 4: case 8:
+				if (bmpinternal->compression == BI_RGB) {
+					hbitmap = img->helper.CreateIndexedDIBSection(hdc, img->width, img->height, &bits, FillBMP, (void*)bmpinternal);
+				} else {
+					hbitmap = img->helper.CreateDefaultDIBSection(hdc, img->width, img->height, "Unsupported compression", &bits);
+					return hbitmap;
+				}
+				break;
+			case 16: case 24: case 32: 
+			if (bmpinternal->compression == BI_RGB) {
+				hbitmap = img->helper.CreateTrueColorDIBSection(hdc, img->width, img->height, &bits, bitcount);
+			} else {
+				hbitmap = img->helper.CreateDefaultDIBSection(hdc, img->width, img->height, "Unsupported compression", &bits);
+			}
+			break;
+		}
+		if (bits) {
+			fseek(file, bmpinternal->offset, SEEK_SET); // goto start of data
+			fread((char*)bits, 1, img->height * realwidth, file); 
+		}
+		fclose(file);
     }
     return hbitmap;
 }
