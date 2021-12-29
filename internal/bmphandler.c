7a36c26068c8e93d4bd3ddbfc927f8099e630415
Frédéric Delhoume <delhoume@gmail.com>
2021-12-29 14:38:33 +0100

    Replace stdio with win32 calls for file handling

--- internal/bmphandler.c
+++ internal/bmphandler.c
@@ -1,9 +1,5 @@
 #include <bmphandler.h>
 
-#include <stdio.h>
-
-// TODO: replace by win32 API
-
 static BOOL AcceptBMPImage(const unsigned char* buffer, unsigned int size) {
     if (size >= 2) {
 	if ((buffer[0] == 'B') && (buffer[1] == 'M')) {
@@ -21,7 +17,7 @@ struct bmp_internal {
     unsigned short bitcount;
     unsigned int compression;
     unsigned int paloffset;
-	FILE* file;
+	HANDLE handle;
 };
 
 static BOOL OpenBMPImage(ImagePtr img, const TCHAR* name) {
@@ -35,15 +31,31 @@ static BOOL OpenBMPImage(ImagePtr img, const TCHAR* name) {
     return TRUE;
 }
 
-static unsigned int ReadInt(FILE* file) {
+static unsigned int ReadInt(HANDLE handle) {
     unsigned int buf;
-    fread((char*)&buf, 1, sizeof(unsigned int), file);
+	DWORD readbytes;
+	ReadFile(handle, &buf, sizeof(unsigned int), &readbytes, 0);
+     return buf;
+}
+
+static unsigned short ReadShort(HANDLE handle) {
+    unsigned short buf;
+	DWORD readbytes;
+ 	ReadFile(handle, &buf, sizeof(unsigned short), &readbytes, 0);
     return buf;
 }
 
-static unsigned short ReadShort(FILE* file) {
-    unsigned short buf;
-    fread((char*)&buf, 1, sizeof(unsigned short), file);
+static unsigned char ReadByte(HANDLE handle) {
+    unsigned char buf;
+	DWORD readbytes;
+ 	ReadFile(handle, &buf, sizeof(unsigned char), &readbytes, 0);
+    return buf;
+}
+
+static RGBQUAD ReadQuad(HANDLE handle) {
+    RGBQUAD buf;
+	DWORD readbytes;
+ 	ReadFile(handle, &buf, sizeof(RGBQUAD), &readbytes, 0);
     return buf;
 }
 
@@ -51,107 +63,117 @@ static unsigned short ReadShort(FILE* file) {
 #define OS2_NEW	    64
 #define WIN_OS2_OLD 12
 #define WIN_V5      124
+#define BI_RGB_COMPRESSION 3
+
+char buffer[100];
 
 static void SetBMPDirectory(ImagePtr img, unsigned int which) {
     struct bmp_internal* bmp_internal = (struct bmp_internal*)img->handler->internal;
-    FILE* file = fopen(img->name, "rb");
-    if (file) {
-	unsigned int offset;
-	unsigned int headersize;
-	fgetc(file);
-	fgetc(file);
-	ReadInt(file); ReadShort(file); ReadShort(file);
-	offset = ReadInt(file);
-	headersize = ReadInt(file);
-	if ((headersize == WIN_NEW || headersize == OS2_NEW || headersize == WIN_V5)) {
-	    img->width = ReadInt(file);
-	    img->height = ReadInt(file);
-	    ReadShort(file); // planes
-	    bmp_internal->bitcount = ReadShort(file);
-	    bmp_internal->compression = ReadInt(file);
-	    ReadInt(file); // size
-	    ReadInt(file); // horizontal resolution
-	    ReadInt(file); // vertical resolution
-	    ReadInt(file); // colors used
-	    ReadInt(file); // important colors
-	}
-	
-	if (headersize != WIN_OS2_OLD) {
-	    unsigned int bytesleft = headersize - 40;
-	    while (bytesleft--) 
-			fgetc(file);
-	}
-	switch (bmp_internal->bitcount) {
-		case 1: case 4: case 8: {
-			RGBQUAD rgb;
-			unsigned int idx;
-			unsigned int palsize = 1 << bmp_internal->bitcount;
-			bmp_internal->paloffset = ftell(file);
-			for (idx = 0; idx < palsize; ++idx) {
-				fread((char*)&rgb, 1, sizeof(RGBQUAD), file);
+    HANDLE handle = CreateFile(img->name, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING,
+			       FILE_ATTRIBUTE_NORMAL, NULL);
+    if (handle) {
+		unsigned int offset;
+		unsigned int headersize;
+//		fgetc(handle); fgetc(handle);
+		ReadByte(handle); ReadByte(handle);
+		ReadInt(handle); ReadShort(handle); ReadShort(handle);
+		offset = ReadInt(handle);
+		headersize = ReadInt(handle);
+		if ((headersize == WIN_NEW || headersize == OS2_NEW || headersize == WIN_V5)) {
+			img->width = ReadInt(handle);
+			img->height = ReadInt(handle);
+			ReadShort(handle); // planes
+			bmp_internal->bitcount = ReadShort(handle);
+			bmp_internal->compression = ReadInt(handle);
+			ReadInt(handle); // size
+			ReadInt(handle); // horizontal resolution
+			ReadInt(handle); // vertical resolution
+			ReadInt(handle); // colors used
+			ReadInt(handle); // important colors
+			wsprintf(buffer, "%dx%d b: %d c: %d", img->width, img->height, bmp_internal->bitcount, bmp_internal->compression);
+			OutputDebugString(buffer);
+		}
+		
+		if (headersize != WIN_OS2_OLD) { // skip remaining header
+			unsigned int bytesleft = headersize - 40;
+			while (bytesleft--) {
+				ReadByte(handle);
 			}
 		}
-	}
-	bmp_internal->offset = ftell(file);
-	img->istiled = FALSE;
-	img->numtilesx = 1;
-	img->numtilesy = 1;
-	img->twidth = img->width;
-	img->theight = img->height;
-	img->subfiletype = Normal;
-	fclose(file);
+		switch (bmp_internal->bitcount) {
+			// skip palette, not used yet
+			case 1: case 4: case 8: {
+				unsigned int idx;
+				unsigned int palsize = 1 << bmp_internal->bitcount;
+				bmp_internal->paloffset = SetFilePointer(handle, 0, 0, FILE_CURRENT);
+				for (idx = 0; idx < palsize; ++idx) {
+					ReadQuad(handle);
+				}
+			}
+		}
+		bmp_internal->offset = SetFilePointer(handle, 0, 0, FILE_CURRENT);
+		img->istiled = FALSE;
+		img->numtilesx = 1;
+		img->numtilesy = 1;
+		img->twidth = img->width;
+		img->theight = img->height;
+		img->subfiletype = Normal;
+		CloseHandle(handle);
     }
 }
 
+
 static void FillBMPPalette(BITMAPINFO* bmi, unsigned int idx, void* arg) {
     struct bmp_internal* bmp_internal = (struct bmp_internal*)arg;
-    FILE* file = (FILE*)bmp_internal->file;
-    fseek(file, bmp_internal->paloffset, SEEK_SET);
+    HANDLE handle = bmp_internal->handle;
+    SetFilePointer(handle, bmp_internal->paloffset, 0, FILE_BEGIN);
     for (unsigned int i = 0; i < idx; ++i) {
-		fread((char*)&(bmi->bmiColors[idx]), 1, sizeof(RGBQUAD), file);
+		bmi->bmiColors[idx] = ReadQuad(handle);
 	}
 }
 
 static HBITMAP 
 LoadBMPTile(ImagePtr img, HDC hdc, unsigned int x, unsigned int y) {
-   FILE* file = fopen(img->name, "rb");
+  HANDLE handle = CreateFile(img->name, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING,
+			       FILE_ATTRIBUTE_NORMAL, NULL);
     HBITMAP hbitmap = 0;
-    if (file) {
+    if (handle) {
 		unsigned int* bits = 0;
 		struct bmp_internal* bmpinternal = (struct bmp_internal*)img->handler->internal;
-		bmpinternal->file = file;
+		bmpinternal->handle = handle;
 		unsigned short bitcount = bmpinternal->bitcount;
 		unsigned int realwidth = (((img->width * bitcount) + 31) & ~31) >> 3;
 		switch (bitcount) {
 			case 1: case 4: case 8:
-				if (bmpinternal->compression == BI_RGB) {
+				if (bmpinternal->compression == BI_RGB_COMPRESSION) {
 					hbitmap = img->helper.CreateIndexedDIBSection(hdc, img->width, img->height, &bits, FillBMPPalette, (void*)bmpinternal);
 				} else {
 					hbitmap = img->helper.CreateDefaultDIBSection(hdc, img->width, img->height, "Unsupported compression", &bits);
-					return hbitmap;
 				}
 				break;
 			case 16: case 24: case 32: 
-			if (bmpinternal->compression == BI_RGB) {
-				hbitmap = img->helper.CreateTrueColorDIBSection(hdc, img->width, img->height, &bits, bitcount);
-			} else {
-				hbitmap = img->helper.CreateDefaultDIBSection(hdc, img->width, img->height, "Unsupported compression", &bits);
-			}
+				if (bmpinternal->compression == BI_RGB_COMPRESSION) {
+					hbitmap = img->helper.CreateTrueColorDIBSection(hdc, img->width, img->height, &bits, bitcount);
+				} else {
+					hbitmap = img->helper.CreateDefaultDIBSection(hdc, img->width, img->height, "Unsupported compression", &bits);
+				}
 			break;
 		}
 		if (bits) {
-			fseek(file, bmpinternal->offset, SEEK_SET); // goto start of data
-			fread((char*)bits, 1, img->height * realwidth, file); 
-		}
-		fclose(file);
-    }
+			DWORD readbytes;
+			SetFilePointer(handle, bmpinternal->offset, 0, FILE_BEGIN);
+			ReadFile(handle, bits, (DWORD)(img->height * realwidth), &readbytes, 0);
+    	}
+		CloseHandle(handle);
+	} else {
+	}
     return hbitmap;
 }
 
 static void CloseBMPImage(ImagePtr img) {
     if (img->handler && img->handler->internal) {
-	struct bmp_internal* bmp_internal = (struct bmp_internal*)img->handler->internal;
-	MYFREE(bmp_internal);
+		struct bmp_internal* bmp_internal = (struct bmp_internal*)img->handler->internal;
+		MYFREE(bmp_internal);
     }
 }
 
